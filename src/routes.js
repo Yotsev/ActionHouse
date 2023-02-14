96f15a9e1ffafa5f5d02456718bebfa2b21858b6
Yotsev <b.yotsev@gmail.com>
2023-02-14 16:38:15 +0200

    Fiexed router

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,8 @@ const router = require('express').Router();
 
 const { authentication } = require('./middlewares/authMiddleware');
 
-const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
-const actionController = require('./controllers/actionController');
 
-router.use('/', homeController);
 router.use('/auth', authController);
-router.use('/action', actionController);
 
 module.exports = router;
